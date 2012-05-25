690425b424bd1fd099b958c5a012b8379834973c
kevin <kevin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-05-25 09:02:38 +0000

    update IMS scripts
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBDBScripts@142 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IMS_Sample_Data.sql
+++ IMS_Sample_Data.sql
@@ -1,33 +1,33 @@
-INSERT INTO lot (`lot_id`,`user_id`,`entity_type`,`entity_id`,`location_id`,`scale_id`,`status`,`source_id`,`comments`)
+INSERT INTO ims_lot (`lotid`,`userid`,`etype`,`eid`,`locid`,`scaleid`,`status`,`sourceid`,`comments`)
 VALUES (-1,1,'GERMPLSM',50533,9000,1538,0,null,'A sample lot');
-INSERT INTO lot (`lot_id`,`user_id`,`entity_type`,`entity_id`,`location_id`,`scale_id`,`status`,`source_id`,`comments`)
+INSERT INTO ims_lot (`lotid`,`userid`,`etype`,`eid`,`locid`,`scaleid`,`status`,`sourceid`,`comments`)
 VALUES (-2,1,'GERMPLSM',1,9000,1538,0,null,'A sample lot');
-INSERT INTO lot (`lot_id`,`user_id`,`entity_type`,`entity_id`,`location_id`,`scale_id`,`status`,`source_id`,`comments`)
+INSERT INTO ims_lot (`lotid`,`userid`,`etype`,`eid`,`locid`,`scaleid`,`status`,`sourceid`,`comments`)
 VALUES (-3,1,'GERMPLSM',50533,9000,1538,0,null,'A sample lot');
-INSERT INTO lot (`lot_id`,`user_id`,`entity_type`,`entity_id`,`location_id`,`scale_id`,`status`,`source_id`,`comments`)
+INSERT INTO ims_lot (`lotid`,`userid`,`etype`,`eid`,`locid`,`scaleid`,`status`,`sourceid`,`comments`)
 VALUES (-4,1,'GERMPLSM',2,9000,1538,0,null,'A sample lot');
-INSERT INTO lot (`lot_id`,`user_id`,`entity_type`,`entity_id`,`location_id`,`scale_id`,`status`,`source_id`,`comments`)
+INSERT INTO ims_lot (`lotid`,`userid`,`etype`,`eid`,`locid`,`scaleid`,`status`,`sourceid`,`comments`)
 VALUES (-5,1,'GERMPLSM',3,9000,1538,0,null,'A sample lot');
 
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-1,1,-1,20120405,1,1000,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-2,1,-2,20120405,1,1000,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-3,1,-3,20120405,1,1000,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-4,1,-4,20120405,1,1000,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-5,1,-5,20120405,1,1000,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-6,1,-1,20120407,1,-200,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-7,1,-1,20120412,0,100,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-8,1,-1,20120410,0,-500,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-9,1,-1,20120408,1,-200,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-10,1,-2,20120408,1,-1000,'sample transaction',NULL,NULL,NULL,NULL,253);
-INSERT INTO transaction (`transaction_id`,`user_id`,`lot_id`,`transaction_date`,`status`,`quantity`,`comments`,`source_type`,`source_id`,`source_record_id`,`previous_amount`,`person_id`)
+INSERT INTO ims_transaction (`trnid`,`userid`,`lotid`,`trndate`,`trnstat`,`trnqty`,`comments`,`sourcetype`,`sourceid`,`recordid`,`prevamount`,`personid`)
 VALUES (-11,1,-3,20120408,1,-850,'sample transaction',NULL,NULL,NULL,NULL,253);
