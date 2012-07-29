81151ed9342cf7e20d53c39eda48669126a136a0
alex <alex@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-07-29 10:27:52 +0000

    alter workbench_project table, add locid and mid fields
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBDBScripts@413 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBDBv1_Workbench_Project.sql
+++ IBDBv1_Workbench_Project.sql
@@ -234,6 +234,8 @@ CREATE TABLE workbench_project (
     ,template_id                INT UNSIGNED NOT NULL
     ,template_modified          BOOL NOT NULL DEFAULT FALSE
     ,crop_type                  ENUM('CHICKPEA', 'COWPEA', 'MAIZE', 'RICE', 'WHEAT', 'CASSAVA')
+    ,locid                		INT UNSIGNED NOT NULL
+    ,mid                		INT UNSIGNED NOT NULL
     ,PRIMARY KEY(project_id)
     ,CONSTRAINT fk_project_1 FOREIGN KEY(template_id) REFERENCES workbench_workflow_template(template_id) ON UPDATE CASCADE
 )
