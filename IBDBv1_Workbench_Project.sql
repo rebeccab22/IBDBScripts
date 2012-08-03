660b5bef23d0f681c1229373a414cf7d98e7dbd4
michael <michael@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-08-03 05:06:56 +0000

    [Michael 8/3/2012] GCP 341 - added last_open_date field for workbench_project
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBDBScripts@443 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBDBv1_Workbench_Project.sql
+++ IBDBv1_Workbench_Project.sql
@@ -236,6 +236,7 @@ CREATE TABLE workbench_project (
     ,crop_type                  ENUM('CHICKPEA', 'COWPEA', 'MAIZE', 'RICE', 'WHEAT', 'CASSAVA')
     ,locid                		INT UNSIGNED NOT NULL
     ,mid                		INT UNSIGNED NOT NULL
+    ,last_open_date             TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
     ,PRIMARY KEY(project_id)
     ,CONSTRAINT fk_project_1 FOREIGN KEY(template_id) REFERENCES workbench_workflow_template(template_id) ON UPDATE CASCADE
 )
