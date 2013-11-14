f5789f1f591159a8ec678d1bb06f63874e5a7204
rebeccab22 <rebeccaberrigan@gmail.com>
2013-11-14 13:19:14 +1300

    Deleting scrips that are now renamed for Ant ordered load

--- IBDBv1_WorkbenchCropData.sql
+++ IBDBv1_WorkbenchCropData.sql
@@ -2,9 +2,9 @@
 -- you only need to do this if you are setting up the workbench database yourself
 -- the workbench/central database installers will initialize
 -- the contents of the workbench_crop table for you.
-INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Cassava', 'ibdb_cassava_central');
-INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Chickpea', 'ibdb_chickpea_central');
-INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Cowpea', 'ibdb_cowpea_central');
-INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Maize', 'ibdb_maize_central');
-INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Rice', 'ibdb_rice_central');
-INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Wheat', 'ibdb_wheat_central');
+INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Cassava', 'ibdbv2_cassava_central');
+INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Chickpea', 'ibdbv2_chickpea_central');
+INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Cowpea', 'ibdbv2_cowpea_central');
+INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Maize', 'ibdbv2_maize_central');
+INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Rice', 'ibdbv2_rice_central');
+INSERT INTO workbench_crop(crop_name, central_db_name) VALUES ('Wheat', 'ibdbv2_wheat_central');
