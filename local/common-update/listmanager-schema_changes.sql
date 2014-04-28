fd8232a918686a53b7b51a18e95411ce0f79f4cb
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-04-28 16:00:18 +0800

    stored proc for adding column if exists (GCP-8381)

--- local/common-update/listmanager-schema_changes.sql
+++ local/common-update/listmanager-schema_changes.sql
@@ -1,3 +1,22 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS add_column_if_not_exists $$
+CREATE PROCEDURE add_column_if_not_exists(in theTable varchar(128), in theColumn varchar(128),in theColumnType )
+BEGIN
+
+ IF(( SELECT COUNT(*) 
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = DATABASE()
+    	AND   TABLE_NAME = theTable 
+        AND   COLUMN_NAME = theColumn) = 0) THEN
+   SET @s = CONCAT('ALTER TABLE ' , theTable , ' ADD COLUMN(`' , theForeignKey,'` ',theColumnType, ')');
+   PREPARE stmt FROM @s;
+   EXECUTE stmt;
+ END IF;
+END $$
+
+DELIMITER ;
+
 DROP TABLE IF EXISTS `listdataprops`;
 
 CREATE TABLE `listdataprops` ( 
@@ -8,6 +27,8 @@ CREATE TABLE `listdataprops` (
   PRIMARY KEY (`listdataprop_id`) 
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-ALTER TABLE `listnms` ADD COLUMN(
-  `notes` TEXT
-);
+-- ALTER TABLE `listnms` ADD COLUMN(
+--   `notes` TEXT
+-- );
+
+CALL add_column_if_not_exists('listnms','notes','TEXT');
