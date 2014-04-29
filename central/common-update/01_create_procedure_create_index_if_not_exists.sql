70b661169280a816dff1b800eda476b6cc4357a3
Glenn Marintes <glenn@efficio.us.com>
2014-04-29 15:12:23 +0800

    formatted procedure code

--- central/common-update/01_create_procedure_create_index_if_not_exists.sql
+++ central/common-update/01_create_procedure_create_index_if_not_exists.sql
@@ -1,14 +1,18 @@
 DELIMITER $$
  
 DROP PROCEDURE IF EXISTS create_index_if_not_exists $$
-CREATE PROCEDURE create_index_if_not_exists(in theIndexName varchar(128), in theTable varchar(128), in theColumnName varchar(128) )
+CREATE PROCEDURE create_index_if_not_exists(
+     IN theIndexName        VARCHAR(128)
+    ,IN theTable            VARCHAR(128)
+    ,IN theColumnName       VARCHAR(128)
+)
 BEGIN
- IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name =
-theTable AND index_name = theIndexName) = 0) THEN
-   SET @s = CONCAT('CREATE INDEX ' , theIndexName , ' ON ' , theTable, ' (', theColumnname,')');
-   PREPARE stmt FROM @s;
-   EXECUTE stmt;
- END IF;
+
+    IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = theTable AND index_name = theIndexName) = 0) THEN
+        SET @s = CONCAT('CREATE INDEX ' , theIndexName , ' ON ' , theTable, ' (', theColumnname,')');
+        PREPARE stmt FROM @s;
+        EXECUTE stmt;
+    END IF;
 END $$
  
 DELIMITER ;
